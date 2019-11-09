76b1acb9555cb1446bc8c279b15f251433a21b89
nejah safi <nejah.essafi@hotmail.fr>
2019-11-09 06:53:37 +0000

    final

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # selfProject
+
+
+My project is about Baby 
+it hold 3 section :vaccination , food  and mama
+in the vaccination section you can display the list of vaccination according to the age of your child
+you can filter the foods if your child has an alergie
+and you can add items to the card
--- baby.js
+++ baby.js
@@ -4,45 +4,81 @@ $("#vaccination-section").click(function() {
     }, 1000);
 });
 
+$("#baby-section").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#baby_food").offset().top
+    }, 1000);
+});
 
+$(function(){
 
-var listvaccin = [
-	{
-		"age" : 0,
-		"vaccin" : ["fklbkldf"]
-	},
-	{ 
-		"age": 1,
-		"vaccin" : ["hhhh", "dfdgd"]
-	}	,
-	{
-		"age": 2,
-		"vaccin" : ["aaa" , "zzzz"]
+    $('.radio').click(function() {
+
+        $('input:checkbox').not(this).prop('checked', false);
+
+    })
+
+});
+
+$(function(){
+	$('input[type=checkbox]').click();
+});
+
+function show (id_disply_none1,id_disply_none2,id_disply_none3,id_disply_block) {
+  
+    	document.getElementById(id_disply_block).style.display= "block";
+    	document.getElementById(id_disply_none1).style.display= "none";
+    	document.getElementById(id_disply_none2).style.display= "none";
+    	document.getElementById(id_disply_none3).style.display= "none";
+    
+    
+}
+
+function filter(){
+document.getElementById('sum').value =0 ;
+document.getElementById('tot').value = 0;
+
+	var x = document.getElementById('myselect').value;
+	if (x == "eggfree"){
+		document.getElementById('egg').style.display= "none";
 	}
-]
+	if (x == "fishfree"){
+		document.getElementById('fish').style.display= "none";
+	}
+	if (x == "glutenfree"){
+		document.getElementById('gluten').style.display= "none";
 
-function getinfo(){
-          
-var radios = document.getElementsByName('gender');
+	}
+	if (x == "dairyfree"){
+		document.getElementById('dairy').style.display= "none";
 
-for (var i = 0, length = radios.length; i < length; i++)
-{
- if (radios[i].checked)
- {
-  // do whatever you want with the checked radio
- var gendervalue = radios[i].value
+	}	
 
-  // only one radio can be logically checked, don't check the rest
-  break;
- }
+
+
 }
 
-            var date = document.getElementById("date-id").value;
-            var age_bebe = document.getElementById("age-id").value;
-/*alert(age_bebe);*/
+var total_prix =0 ;
 
-
-            
-/*var baby = {sexe: gendervalue , age = "white"};*/
+function increment(price){ 
+		  
+		   var qtt= document.getElementById('sum')
+          document.getElementById('sum').value= (Number(qtt.value))+1;
+          var  total_price = document.getElementById("tot")
+		 document.getElementById('tot').value = (Number(total_price.value))+price;
+		        
+        } 
+function deletecard(){
+	document.getElementById('sum').value =0 ;
+document.getElementById('tot').value = 0;
 }
+
+
+$(document).ready(function(){
+
+    $("#a1").hide();
+    $("#a2").hide();
+    $("#a3").hide();
+    $("#a4").hide();
+});
 
